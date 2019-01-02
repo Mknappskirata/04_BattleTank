bab146c6ac9bdfa756a4822897ab7e65c93bb2ca
DESKTOP-L1VRQGA\Owner <mknappskirata@gmail.com>
2019-01-02 17:10:46 -0600

    Video 128

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BattleTank.h"
 #include "Tank.h"
+#include "BattleTank.h"
+
 
 
 // Sets default values
--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,7 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BattleTank.h"
 #include "TankPlayerController.h"
+#include "BattleTank.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not possessing"));
+
+	}
+
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possessing %s"), *(ControlledTank->GetName()));
+
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin play"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
