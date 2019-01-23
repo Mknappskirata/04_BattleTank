a6e5be2ff36ba5232763671e65714c17599069cb
DESKTOP-L1VRQGA\Owner <mknappskirata@gmail.com>
2019-01-22 23:01:49 -0600

    Video 158 Projectile Movement

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -22,17 +22,18 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Fire projectile"), Time);
-
 	if (!Barrel) { return; }
 	
 	//spawn a projectile on socket location on the barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 			ProjectileBlueprint,
 			Barrel->GetSocketLocation(FName("Projectile")),
 			Barrel->GetSocketRotation(FName("Projectile"))
 		);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
+
+	
 }
 
 // Sets default values
